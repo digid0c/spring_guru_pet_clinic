2421f7f619820119fcd77c3e95e7196e8b8dd338
digid0c <digidoc22@gmail.com>
2020-01-20 13:19:02 +0200

    Refactor service interfaces to inherit from common base service. Closes #11.

---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/CrudService.java
@@ -0,0 +1,16 @@
+package guru.samples.petclinic.service;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T entity);
+
+    void delete(T entity);
+
+    void deleteById(ID id);
+}
