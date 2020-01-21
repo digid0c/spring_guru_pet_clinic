3ba2ee6b6fd6cce6459e6427f08d34840839fe6e
digid0c <digidoc22@gmail.com>
2020-01-21 15:29:20 +0200

    List all vets on vet index page. Closes #15.

--- pet_clinic_web/src/main/java/guru/samples/petclinic/controller/OwnerController.java
+++ pet_clinic_web/src/main/java/guru/samples/petclinic/controller/OwnerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
-    private OwnerService ownerService;
+    private final OwnerService ownerService;
 
     @Autowired
     public OwnerController(OwnerService ownerService) {
