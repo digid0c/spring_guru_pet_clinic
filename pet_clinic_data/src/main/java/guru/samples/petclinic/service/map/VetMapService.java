ee330b84e5ed124b7c89d8f34ae96cb02ba8ba7f
digid0c <digidoc22@gmail.com>
2020-01-26 17:20:54 +0200

    Add specialties to vets on startup. Closes #34.

--- pet_clinic_data/src/main/java/guru/samples/petclinic/service/map/VetMapService.java
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/map/VetMapService.java
@@ -1,13 +1,22 @@
 package guru.samples.petclinic.service.map;
 
 import guru.samples.petclinic.model.Vet;
+import guru.samples.petclinic.service.SpecialityService;
 import guru.samples.petclinic.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    @Autowired
+    public VetMapService(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +30,16 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (vet == null) {
+            return null;
+        }
+
+        vet.getSpecialities().forEach(speciality -> {
+            if (speciality.isNew()) {
+                specialityService.save(speciality);
+            }
+        });
+
         return super.save(vet);
     }
 
--- pet_clinic_web/src/main/java/guru/samples/petclinic/bootstrap/DataLoader.java
+++ pet_clinic_web/src/main/java/guru/samples/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package guru.samples.petclinic.bootstrap;
 
-import guru.samples.petclinic.model.Owner;
-import guru.samples.petclinic.model.Pet;
-import guru.samples.petclinic.model.PetType;
-import guru.samples.petclinic.model.Vet;
+import guru.samples.petclinic.model.*;
 import guru.samples.petclinic.service.OwnerService;
 import guru.samples.petclinic.service.PetTypeService;
+import guru.samples.petclinic.service.SpecialityService;
 import guru.samples.petclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,16 +17,25 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -38,6 +45,20 @@ public class DataLoader implements CommandLineRunner {
         PetType savedCatPetType = petTypeService.save(cat);
 
         System.out.println("Loaded Pet Types...");
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
+        System.out.println("Loaded Specialties...");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -76,12 +97,14 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedSurgery);
 
         vetService.save(vet2);
 
