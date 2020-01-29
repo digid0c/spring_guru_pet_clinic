11631f83d882b95be8cfe5e90518c278800285b7
digid0c <digidoc22@gmail.com>
2020-01-29 19:27:12 +0200

    Add Visit map based service. Closes #46.

---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/VisitService.java
@@ -0,0 +1,7 @@
+package guru.samples.petclinic.service;
+
+import guru.samples.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package guru.samples.petclinic.service.map;
+
+import guru.samples.petclinic.model.Visit;
+import guru.samples.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit == null) {
+            return null;
+        } else if (visit.getPet() == null || visit.getPet().isNew()
+                || visit.getPet().getOwner() == null || visit.getPet().getOwner().isNew()) {
+            throw new RuntimeException("Pet and pet owner are required.");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
