196888ecdb6a8cd3787637dda683d5e444818a41
digid0c <digidoc22@gmail.com>
2020-01-29 14:47:16 +0200

    Add Speciality JPA service. Closes #44.

---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/jpa/SpecialityJpaService.java
@@ -0,0 +1,48 @@
+package guru.samples.petclinic.service.jpa;
+
+import guru.samples.petclinic.model.Speciality;
+import guru.samples.petclinic.repository.SpecialityRepository;
+import guru.samples.petclinic.service.SpecialityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class SpecialityJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    @Autowired
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return StreamSupport.stream(specialityRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
