6b22368a843fe15da0a3467daa79cd4744bdf1b8
digid0c <digidoc22@gmail.com>
2020-01-29 13:59:07 +0200

    Add PetType JPA service. Closes #42.

---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/jpa/PetTypeJpaService.java
@@ -0,0 +1,48 @@
+package guru.samples.petclinic.service.jpa;
+
+import guru.samples.petclinic.model.PetType;
+import guru.samples.petclinic.repository.PetTypeRepository;
+import guru.samples.petclinic.service.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return StreamSupport.stream(petTypeRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
