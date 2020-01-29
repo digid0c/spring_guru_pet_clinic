b763c5ec0838c1dd91e83d72c5f5be4e1e3a9713
digid0c <digidoc22@gmail.com>
2020-01-29 14:31:41 +0200

    Add Pet JPA service. Closes #43.

---
+++ pet_clinic_data/src/main/java/guru/samples/petclinic/service/jpa/PetJpaService.java
@@ -0,0 +1,48 @@
+package guru.samples.petclinic.service.jpa;
+
+import guru.samples.petclinic.model.Pet;
+import guru.samples.petclinic.repository.PetRepository;
+import guru.samples.petclinic.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    @Autowired
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return StreamSupport.stream(petRepository.findAll().spliterator(), false)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
