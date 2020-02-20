5bb86669f812674e3b63298d5d1a3f75774b06b5
digid0c <digidoc22@gmail.com>
2020-02-20 23:51:49 +0200

    Create pet controller, add common model attributes. #56

---
+++ pet_clinic_web/src/main/java/guru/samples/petclinic/controller/PetController.java
@@ -0,0 +1,49 @@
+package guru.samples.petclinic.controller;
+
+import guru.samples.petclinic.model.Owner;
+import guru.samples.petclinic.model.PetType;
+import guru.samples.petclinic.service.OwnerService;
+import guru.samples.petclinic.service.PetService;
+import guru.samples.petclinic.service.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String PET_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final OwnerService ownerService;
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    @Autowired
+    public PetController(OwnerService ownerService, PetService petService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+}
